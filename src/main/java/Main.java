411a316c7d701673ee8d040f1eae66b27ba0e13b
Anirudha shukla <anirudhashukla0807@gmail.com>
2026-08-24 22:28:05 +0530

    major refactoring and added error handling , search ,sorting and menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,123 @@
 import java.util.*;
+
 public class Main {
-    
-    public static void displayuserinfo(User user){
-        System.out.println("-********************** User information **********************");
-        System.out.println("Usere login: "+user.login);
-        System.out.println("name: "+user.name);
-        System.out.println("followers: "+user.followers);
-        System.out.println("no of repos"+user.publicRepos);
+
+    public static void displayuserinfo(User user) {
+        System.out.println(" ********************** User information **********************");
+        System.out.println("Usere login: " + user.login);
+        System.out.println("name: " + user.name);
+        System.out.println("followers: " + user.followers);
+        System.out.println("no of repos : " + user.publicRepos);
 
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter a username : ");
         String username=sc.nextLine();
         username = username.trim();
-        sc.close();
         if (username.isBlank()) {
         System.out.println("Username cannot be empty.");
         return;
         }
         try{User user = GitHubServices.getuserinfo(username);
         List<Repository> repositories = GitHubServices.getRepositories(username);
-        displayuserinfo(user);
-        RepositoryAnalysis.analysis(repositories);}
+        boolean running = true;
+        while(running){
+        System.out.println("\n========== GitHub Analyzer ==========");
+        System.out.println("1. Show User Information");
+        System.out.println("2. Show Repository Analysis");
+        System.out.println("3. Search Repository");
+        System.out.println("4. Search Repositories by Language");
+        System.out.println("5. Sort Repository : ");
+        System.out.println("6. Exit\n");
+        System.out.println("Enter choice : ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+            switch (choice) {
+                case 1:
+                    displayuserinfo(user);
+                    break;
+                case 2:
+                    RepositoryAnalysis.analysis(repositories);
+                    break;
+                case 3:
+                    System.out.println("Enter the repository name you want to search :");
+                    String repoName =sc.nextLine();
+                    repoName=repoName.trim();
+                    Repository repo=RepositoryAnalysis.findRepository(repositories,repoName);
+                    if(repo!=null){
+                        System.out.println("Repository name : "+repo.name);
+                        System.out.println("Repository description: "+repo.description);
+                        System.out.println("Language used : "+repo.language);
+                        System.out.println("stars : "+repo.stars);
+                        System.out.println("forks: "+repo.forks);
+                        
+                    }else{
+                        System.out.println("Repository not found !");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter the repository language you want to search : ");
+                    String repoLang=sc.nextLine();
+                    repoLang=repoLang.trim();
+                    List<Repository> langList=RepositoryAnalysis.findByLanguage(repositories, repoLang);
+                    if(langList.isEmpty()){
+                        System.out.println("None of the Repositories use the language : "+repoLang);
+                    }else{
+                    for(Repository repo1:langList){
+                        System.out.println("Repository name : "+repo1.name);
+                        System.out.println("Repository description: "+repo1.description);
+                        System.out.println("stars : "+repo1.stars);
+                        System.out.println("forks: "+repo1.forks);
+                    }
+                    }
+                    break;
+                case 5:
+                    System.out.println("1. Sort Repositories by Stars ");
+                    System.out.println("2. Sort Repositories by forks ");
+                    System.out.println("3. Sort Repositories Alphabetically ");
+                    System.out.print("choose a type : ");
+
+                    int type=sc.nextInt();
+                    sc.nextLine();
+                    switch (type) {
+                        case 1:
+                            repositories.sort((a,b)->b.stars-a.stars);
+                            for(int i =0;i<Math.min(5, repositories.size());i++){
+                                Repository repo2 = repositories.get(i);
+                                System.out.println((i+1)+". "+repo2.name+"-->"+repo2.stars);
+                            }
+                            break;
+                        case 2:
+                            repositories.sort((a,b)->b.forks-a.forks);
+                            for(int i =0;i<Math.min(5, repositories.size());i++){
+                                Repository repo2 = repositories.get(i);
+                                System.out.println((i+1)+". "+repo2.name+"-->"+repo2.forks);
+                            }
+                            break;
+                        case 3:
+                            repositories.sort((a,b)->a.name.compareToIgnoreCase(b.name));
+                            for(int i =0;i<repositories.size();i++){
+                                Repository repo2 = repositories.get(i);
+                                System.out.println(((i+1)+". "+repo2.name));
+                            }
+                            break;        
+                    }
+                    break;
+                case 6:
+                    running=false; 
+                    break;   
+                default:
+                    break;
+            }
+        }
+        }
         catch(Exception e){
             System.out.println(e.getMessage());
 
         }
-
-   
-}
     }
+}
+        
+
--- src/main/java/RepositoryAnalysis.java
+++ src/main/java/RepositoryAnalysis.java
@@ -1,15 +1,39 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RepositoryAnalysis {
-    public static void analysis(List<Repository> repositories)throws Exception{
+    public static List<Repository> findByLanguage(List<Repository> repositories, String lang){
+        List<Repository> langList=new ArrayList<>();
+        for(Repository repo:repositories){
+            if(repo.language!=null && repo.language.equalsIgnoreCase(lang)){
+                langList.add(repo);
+            }
+        }
+        return langList;
+    }
+    public static Repository findRepository(List<Repository> repositories, String repoName){
+        for(Repository repo:repositories){
+            if(repo.name.equalsIgnoreCase(repoName)){
+                return repo;
+            }
+        }
+        return null;
+    }
+    public static void analysis(List<Repository> repositories){
         int maxStars=0;
+        int maxFork=0;
+        String maxForkRepo="";
         String maxRepo="";
         int totalStars=0;
         int totalRepositories=repositories.size();
         HashMap<String,Integer> hmap=new HashMap<>();
         for (Repository repo : repositories) {
+            if(repo.forks>maxFork){
+                maxFork=repo.forks;
+                maxForkRepo=repo.name;
+            }
             totalStars += repo.stars;
             if (repo.stars > maxStars) {
                 maxStars = repo.stars;
@@ -24,10 +48,10 @@ public class RepositoryAnalysis {
             }
         }
         String maxLang="";
-        int countMaxlang=0;
+        int countMaxLang=0;
         for(Map.Entry<String,Integer> entry : hmap.entrySet()){
-            if(entry.getValue()>countMaxlang){
-                countMaxlang=entry.getValue();
+            if(entry.getValue()>countMaxLang){
+                countMaxLang=entry.getValue();
                 maxLang=entry.getKey();
             }
             
@@ -39,17 +63,33 @@ public class RepositoryAnalysis {
         }else{
             avgStars=(double)totalStars/totalRepositories;
         }
+        double highestForkToStar=0;
+        String repoHighestForkToStarRatio="";
+        
         System.out.println("********************** Profile Analysis **********************");
         System.out.println(maxRepo+"is the most starred repo of the user with "+maxStars+" stars");
-        System.out.println("The language most used by the user is "+maxLang+" and number of times it used is : "+countMaxlang);
+        System.out.println(maxForkRepo+"is the most forked repo of the user with "+maxFork+" forks");
+        System.out.println("The language most used by the user is "+maxLang+" and number of times it used is : "+countMaxLang);
         System.out.println("Numbe of Repositories are : "+totalRepositories+"\nTotal stars across all repositories : "+totalStars);
         System.out.println("Average stars : "+avgStars);
         System.out.println("\n*********************** Repository Information **********************");
         System.out.println("\nTOP 5 REPOSITORIES BY STARS\n");
         for(int i =0;i<Math.min(5, repositories.size());i++){ 
             Repository repo=repositories.get(i);
-            System.out.println((i+1+"."+repo.name+"-->"+repo.stars));
+            
+            if(repo.stars!=0){
+                if((double)repo.forks/repo.stars>highestForkToStar){
+                    highestForkToStar=(double)repo.forks/repo.stars;
+                    repoHighestForkToStarRatio=repo.name;
+                }
+                System.out.println((i+1+"."+repo.name+"-->"+repo.stars));
+                System.out.println("Fork to Stars ratio : "+(double)repo.forks/repo.stars);
+            }else{
+                System.out.println("Fork to Stars ratio : (N/A) ");
+            }
+            
         }
+        System.out.println("\nRepository with the highest Fork to Star ratio : "+highestForkToStar+ " is : "+repoHighestForkToStarRatio);        
 
     }
     
